de9f3683d511de03e1aa69273c2cde574814344a
Administrator <YJSong411411>
2016-11-03 17:36:36 +0800

    更新进度

---
+++ myfunctiontest/src/main/java/UpdateProgressTest/ProgressInterface.java
@@ -0,0 +1,9 @@
+package UpdateProgressTest;
+
+/**
+ * Created by Administrator on 2016/11/3.
+ */
+
+public interface ProgressInterface {
+    public void updateProgress(int progress);
+}
---
+++ myfunctiontest/src/main/java/UpdateProgressTest/UpdateService.java
@@ -0,0 +1,32 @@
+package UpdateProgressTest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2016/11/3.
+ */
+
+public class UpdateService extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void startDownload(){
+
+    }
+}
